4f11154b00bb17f2d2c2f94863cd3a073d22a75f
Geez3r <mebenton@gmail.com>
2019-06-25 10:47:05 -0400

    Updates to example

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,37 +1,44 @@
 <template>
 	<v-app>
-		<div>
-			<v-tabs v-model="active">
-				<v-tabs-bar class="cyan" dark>
-					<v-tabs-item ripple :href="'form'">Form</v-tabs-item>
-					<v-tabs-item ripple :href="'model'">Model</v-tabs-item>
-					<v-tabs-item ripple :href="'schema'">Schema</v-tabs-item>
-					<v-tabs-item ripple :href="'options'">Options</v-tabs-item>
-					<v-tabs-slider color="yellow"></v-tabs-slider>
-				</v-tabs-bar>
-				<v-tabs-items class="ma-5">
-					<v-tabs-content :id="'form'">
-						<v-form-generator 
-							:model="model" 
-							:schema="schema" 
-							:options="options"
-							@blur="onBlur"
-							@change="onChange"
-							@focus="onFocus"
-							@input="onInput"/>
-					</v-tabs-content>
-					<v-tabs-content :id="'model'">
-						<pre>{{model}}</pre>
-					</v-tabs-content>
-					<v-tabs-content :id="'schema'">
-						<pre>{{schema}}</pre>
-					</v-tabs-content>
-					<v-tabs-content :id="'options'">
-						<pre>{{options}}</pre>
-					</v-tabs-content>
-				</v-tabs-items>
-			</v-tabs>
-		</div>
+		<v-container fluid>
+			<v-layout>
+				<v-flex xs12>
+					<v-tabs v-model="active">
+						<v-tab ripple key="form">Form</v-tab>
+						<v-tab ripple key="model">Model</v-tab>
+						<v-tab ripple key="schema">Schema</v-tab>
+						<v-tab-item key="form">
+							<v-card flat>
+								<v-card-text>
+									<v-form-generator 
+										:model="model" 
+										:schema="schema" 
+										:options="options"
+										@blur="onBlur"
+										@change="onChange"
+										@focus="onFocus"
+										@input="onInput"/>
+								</v-card-text>
+							</v-card>
+						</v-tab-item>
+						<v-tab-item key="model">
+							<v-card flat>
+								<v-card-text>
+									<pre>{{ model }}</pre>
+								</v-card-text>
+							</v-card>
+						</v-tab-item>
+						<v-tab-item key="schema">
+							<v-card flat>
+								<v-card-text>
+									<pre>{{ schema }}</pre>
+								</v-card-text>
+							</v-card>
+						</v-tab-item>
+					</v-tabs>
+				</v-flex>
+			</v-layout>
+		</v-container>
 	</v-app>
 </template>
 
@@ -43,6 +50,7 @@
 		data(){
 			return {
 				model: {
+					active: null,
 					id: 1,
 					name: "John Doe",
 					password: "J0hnD03!x4",
@@ -53,139 +61,239 @@
 					showHiddenField: true,
 					skillsRadio: null,
 					switch: false,
-					age: 30
+					age: 30,
+					mvrStatus: '',
+					mvrDate: new Date().toISOString().substr(0, 10),
+					menu2: false,
+					firstName: '',
+					lastName: '',
+					driverPanel: [true]
 				},
 				schema: {
-					fields: [{
-						type: "number",
-						label: "ID (disabled text field)",
-						model: "id",
-						readonly: true, 
-						disabled: true,
-						visible: true
-					},{
-						type: "number",
-						label: "Hidden Field",
-						model: "idHidden",
-						readonly: true, 
-						disabled: true,
-						visible: false
-					},{
-						type: "text",
-						label: "Name",
-						model: "name",
-						placeholder: "Your name",
-						featured: true,
-						required: true
-					},{
-						type: "number",
-						label: "Age",
-						model: "age",
-					},{
-						type: "checkbox",
-						label: "Use password?",
-						model: "usePassword"
-					}, {
-						type: "password",
-						label: "Password",
-						model: "password",
-						disabled: false,
-						min: 6,
-						required: true,
-						hint: "Minimum 6 characters",
-						passwordVisible: false
-					},{
-						type: "select",
-						label: "Skills",
-						model: "skills",
-						values: ["Javascript", "VueJS", "CSS3", "HTML5"]
-					},{
-						type: "radio",
-						label: "Select a skill",
-						model: "skillsRadio",
-						values: [
-							{
-								"label": "Javascripts",
-								"value": "1"
-							},
-							{
-								"label": "VueJS",
-								"value": "2"
-							},
-							{
-								"label": "CSS3",
-								"value": "3"
-							}
-						]
-					},
-					{
-						type: "switch",
-						label: "Switch ON/OFF",
-						model: "switch"
-					},
-					{
-						type: "email",
-						label: "E-mail",
-						model: "email",
-						placeholder: "User's e-mail address"
-					},{
-						type: "checkbox",
-						label: "Status",
-						model: "status",
-						default: true
-					},{
-						type: "checkbox",
-						label: "Show next field?",
-						model: "showHiddenField"
-					},{
-						type: "text",
-						label: "Hidden Field",
-						model: "conditionlHiddenField",
-						placeholder: "Some text",
-						visible: (model, field) => model.showHiddenField === true,
-					}],
-					groups: [
+					items: [
 						{
-							key: "userDetails",
-							legend: "User Details",
-							fields: [
+							category: "field",
+							type: "number",
+							label: "ID (disabled text field)",
+							model: "id",
+							readonly: true, 
+							disabled: true,
+							visible: true
+						},{
+							category: "field",
+							type: "number",
+							label: "Hidden Field",
+							model: "idHidden",
+							readonly: true, 
+							disabled: true,
+							visible: false
+						},{
+							category: "field",
+							type: "text",
+							label: "Name",
+							model: "name",
+							placeholder: "Your name",
+							featured: true,
+							required: true
+						},{
+							category: "field",
+							type: "number",
+							label: "Age",
+							model: "age",
+						},{
+							category: "field",
+							type: "checkbox",
+							label: "Use password?",
+							model: "usePassword"
+						}, {
+							category: "field",
+							type: "password",
+							label: "Password",
+							model: "password",
+							disabled: false,
+							min: 6,
+							required: true,
+							hint: "Minimum 6 characters",
+							passwordVisible: false
+						},{
+							category: "field",
+							type: "select",
+							label: "Skills",
+							model: "skills",
+							values: ["Javascript", "VueJS", "CSS3", "HTML5"]
+						},{
+							category: "field",
+							type: "radio",
+							label: "Select a skill",
+							model: "skillsRadio",
+							values: [
 								{
-									type: "textarea",
-									label: "My story",
-									model: "story"
+									"label": "Javascripts",
+									"value": "1"
 								},
 								{
-									type: "email",
-									id: "email",
-									label: "Email Address",
-									model: "email"
+									"label": "VueJS",
+									"value": "2"
+								},
+								{
+									"label": "CSS3",
+									"value": "3"
 								}
 							]
 						},
 						{
-							key: "preferences",
-							legend: "Preferences",
-							fields: [
+							category: "field",
+							type: "switch",
+							label: "Email field is disabled",
+							model: "switch"
+						},
+						{
+							category: "field",
+							type: "email",
+							label: "E-mail",
+							model: "email",
+							placeholder: "User's e-mail address",
+							disabled: (model, field) => model.switch === true
+						},{
+							category: "field",
+							type: "checkbox",
+							label: "Status",
+							model: "status",
+							default: true
+						},{
+							category: "field",
+							type: "checkbox",
+							label: "Show next field?",
+							model: "showHiddenField"
+						},{
+							category: "field",
+							type: "text",
+							label: "Hidden Field",
+							model: "conditionlHiddenField",
+							placeholder: "Some text",
+							visible: "(model, field) => model.showHiddenField === true",
+						},{
+							category: "group",
+							type: "expansionpanels",
+							model: "driverPanel",
+							panels: [
 								{
-									type: "select",
-									label: "Color",
-									model: "color",
-									values: [
-										"Red",
-										"Green",
-										"Blue"
+									key: "driverOne",
+									label: (model, field) => `Driver 1 ${model.firstName} ${model.lastName}`,
+									items: [
+										{
+											category: "field",
+											type: "text",
+											label: "First Name",
+											seperateLabel: true,
+											model: "firstName",
+										},
+										{
+											category: "field",
+											type: "text",
+											label: "Last Name",
+											seperateLabel: true,
+											model: "lastName",
+										}
 									]
-								},
+								}
+							]
+						},{
+							category: "group",
+							type: "cards",
+							cards: [
 								{
-									type: "number",
-									id: "timeout",
-									label: "Timeout in Seconds",
-									model: "timeout"
+									key: "policyDetails",
+									label: "Policy Details",
+									items: [
+										{
+											category: "field",
+											type: "select",
+											label: "Whats the status of the MVR report?",
+											seperateLabel: true,
+											hint: "Heres a persistentHint = true. Select Received for a surprise!",
+											persistentHint: true,
+											singleLine: true,
+											clearable: true,
+											model: "mvrStatus",
+											values: ["Not ordered", "Ordered", "Received"]
+										},
+										{
+											category: "field",
+											type: "datepicker",
+											label: "MVR Ordered Date",
+											seperateLabel: true,
+											model: "mvrDate",
+											visible: (model, field) => model.mvrStatus == 'Received'
+										}
+									]
+								}
+							]
+						},{
+							category: "group",
+							type: "cards",
+							cards: [
+								{
+									key: "locationDetails",
+									label: "Location Details",
+									items: [
+										{
+											category: "group",
+											type: "tabs",
+											tabs: [
+												{
+													key: "location1Details",
+													label: "Location 1",
+													items: [
+														{
+															category: "field",
+															type: "textarea",
+															id: "myStory",
+															label: "My story",
+															model: "story"
+														},
+														{
+															category: "field",
+															type: "email",
+															id: "email",
+															label: "Email Address",
+															model: "email"
+														}
+													]
+												},
+												{
+													key: "location2Details",
+													label: "Location 2",
+													items: [
+														{
+															category: "field",
+															type: "select",
+															label: "Color",
+															model: "color",
+															values: [
+																"Red",
+																"Green",
+																"Blue"
+															]
+														},
+														{
+															category: "field",
+															type: "number",
+															id: "timeout",
+															label: "Timeout in Seconds",
+															model: "timeout"
+														}
+													]
+												}
+											]
+										}
+									]
 								}
 							]
 						}
 					]
 				},
+				
 				options: {
 
 				}
